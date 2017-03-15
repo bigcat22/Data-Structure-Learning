4a8a0758fd2dbdb9146651d007b2f256c91c7d01
Zixuan Hu <hzx4713972@126.com>
2017-03-15 16:51:34 +0800

    Add files via upload

---
+++ 线性表的链式存储结构(Linear List by link)/2.4 线性表的应用/func.c
@@ -0,0 +1,31 @@
+﻿/* fun.c -- 支持连接的函数 */
+#include <stdio.h>
+#include <stdlib.h>
+#include "linktable.h"
+
+#ifndef MaxCol
+	#define MaxCol 10 					//最大列数
+#endif
+
+/* 创建单链表，采用尾插法。用户先输入表的行数和列数，然后输入各行的数据 */
+void CreateTable (HList * &h)
+{
+	int i, j;
+	DList *r, *s;
+	h = (HList *)malloc(sizeof(HList));				//创建头结点
+	printf ("表的行数，列数：");
+	scanf ("%d %d", &h->Row, &h->Col);		//输入表的行数和列数
+	for (i = 0; i < h->Row; i++)						//输入所有行的数据
+	{
+		printf ("第 %d 行:", i+1);
+		s = (DList *)malloc(sizeof(DList));			//创建数据节点
+		for (j = 0; j < h->Col; j++)					//输入一行的数据
+			scanf ("%d", &s->Col; j++);
+		if (h->next == NULL)							//插入第一个数据节点
+			h->next = s;										
+		else														//插入其他数据节点
+			r->next = s;										//将*s插入到*r节点之后
+		r = s;													//r始终指向尾节点
+	}
+	r->next = NULL;										//尾节点的next域置空
+}
---
+++ 线性表的链式存储结构(Linear List by link)/2.4 线性表的应用/linktable.h
@@ -0,0 +1,29 @@
+﻿#define MaxCol 10 					//最大列数
+typedef int ElemType;
+
+/* 单链表的数据节点类型定义 */
+typedef struct Node1				//定义数据节点类型
+{
+	ElemType data[MaxCol];		//存放一行的数据
+	struct Node1 * next;				//指向后继数据节点
+} DList;										//行节点类型
+
+/* 每个表的行数和列数 */
+typedef struct Node2
+{
+	int Row, Col;							//行数和列数
+	DList * next;							//指向第一个数据节点
+} HList;										//头结点类型
+
+/* 函数声明 */
+/* 建表 */
+void CreateTable (HList * &h);		
+
+/* 销毁单链表 */
+void DestoryTable (HList * &h);
+
+/* 输出单链表 */
+void DispTable (HList *h);
+
+/* 表示连接运算 */
+void LinkTable (HList *h1, HList *h2, HList * &h);
