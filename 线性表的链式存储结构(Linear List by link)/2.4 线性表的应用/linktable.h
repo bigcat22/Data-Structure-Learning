860c41e41e9179bdecd603c381a0dabd0f81784d
Zixuan Hu <hzx4713972@126.com>
2017-03-15 16:52:25 +0800

    Update linktable.h

--- 线性表的链式存储结构(Linear List by link)/2.4 线性表的应用/linktable.h
+++ 线性表的链式存储结构(Linear List by link)/2.4 线性表的应用/linktable.h
@@ -1,19 +1,19 @@
-﻿#define MaxCol 10 					//最大列数
+﻿#define MaxCol 10 			//最大列数
 typedef int ElemType;
 
 /* 单链表的数据节点类型定义 */
-typedef struct Node1				//定义数据节点类型
+typedef struct Node1			//定义数据节点类型
 {
 	ElemType data[MaxCol];		//存放一行的数据
-	struct Node1 * next;				//指向后继数据节点
-} DList;										//行节点类型
+	struct Node1 * next;		//指向后继数据节点
+} DList;				//行节点类型
 
 /* 每个表的行数和列数 */
 typedef struct Node2
 {
-	int Row, Col;							//行数和列数
-	DList * next;							//指向第一个数据节点
-} HList;										//头结点类型
+	int Row, Col;			//行数和列数
+	DList * next;			//指向第一个数据节点
+} HList;				//头结点类型
 
 /* 函数声明 */
 /* 建表 */
